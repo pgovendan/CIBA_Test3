837b6c2736bd15578d0f300a6f7a89acb6b19c27
pgovendan <81157415+pgovendan@users.noreply.github.com>
2021-04-09 20:00:48 +0530

    Update Simmdl.m

--- Simmdl.m
+++ Simmdl.m
@@ -4,20 +4,19 @@
 
 function [ret]=Simmdl(Model_Name,MFile_name,file_path)
 try
-	bdclose all;    
-    
+	bdclose all;        
 	evalin('base', MFile_name);
 	simOut = sim(Model_Name)  %  Error code generation 
 	disp('Model Simulation completed successfully')	 
-    ret = 0;
+        ret = 0;
 	exit(0);
 	
 catch e
     disp('Error While simulating the model')
-	fid = fopen(file_path, 'w');
+    fid = fopen(file_path, 'w');
     fprintf(fid,'%s\n',e.message);  
     fclose(fid);
     ret = 1;
-	exit(1);
+    exit(1);
 end
 end
